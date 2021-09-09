65c87b2e1c92827269242bcd42ad0c6b3f8b3056
unknown <mm.ashjaei@gmail.com>
2021-09-09 13:46:36 +0430

    complete

--- RequestWithDatabase.py
+++ RequestWithDatabase.py
@@ -1,13 +1,23 @@
 import requests 
 import json
+import time
 
-MyUrl = "https://api.openweathermap.org/data/2.5/weather"
-MyParams = {'q' : 'tehran' , 'appid' : 'fe8a34a39bab345ee66884db604a92a5'}
-print('fsdssd')
-response = requests.get(url= MyUrl, params= MyParams )
+def reqF (loc):
+    for count in range (5):
+        MyUrl = "https://api.openweathermap.org/data/2.5/weather"
+        MyParams = {'q' : str(location) , 'appid' : 'fe8a34a39bab345ee66884db604a92a5'}
+        response = requests.get(url= MyUrl, params= MyParams ,timeout=3.05)
+        data =json.loads(response.text)
+        #print(data)
+        print ("---------{}---------".format(count))
+        print("temp :      {}".format(data['main']['temp']))
+        print("humidity :  {}".format(data['main']['humidity']))
+        print("time :      {}".format(time.ctime(int(data['dt']))))
+        time.sleep(5)
 
-data =json.loads(response.text)
-print(data)
-
-
-        
+while True :
+    location = input('Enter Your City (for exit type end)...: ')
+    if location == 'end' :
+        break
+    print("Location is {}".format(location))
+    reqF(location)
