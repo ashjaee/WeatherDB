4cb2bdc42addd926398598b0da54c35ccbc0b603
unknown <mm.ashjaei@gmail.com>
2021-09-09 15:03:45 +0430

    my final code 14000618

--- RequestWithDatabase.py
+++ RequestWithDatabase.py
@@ -3,18 +3,15 @@ import json
 import time
 import psycopg2
 
-def reqF (loc):
-    for count in range (5):
+def reqF (loc,loop):
+    for count in range (loop):
         MyUrl = "https://api.openweathermap.org/data/2.5/weather"
         MyParams = {'q' : str(location) , 'appid' : 'fe8a34a39bab345ee66884db604a92a5'}
-        response = requests.get(url= MyUrl, params= MyParams ,timeout=3.05)
+        response = requests.get(url= MyUrl, params= MyParams ,timeout=10)
         data =json.loads(response.text)
-        #print(data)
-        print ("---------{}---------".format(count))
-        print("temp :      {}".format(data['main']['temp']))
-        print("humidity :  {}".format(data['main']['humidity']))
-        print("time :      {}".format(time.ctime(int(data['dt']))))
-        time.sleep(5)
+        print('---',str(count+1),'/',str(loop),'---  registed...')
+        time.sleep(2)
+        regDb(location,data['main']['temp'],data['main']['humidity'])
 
 def regDb(city,tem,hud):
     try:
@@ -23,24 +20,18 @@ def regDb(city,tem,hud):
         user = 'postgres',
         password = '2727',
         database = 'ashjaeiDb')
-        print("Connection to database succeeded...")
+        #print("Connection to database succeeded...")
+        with conn.cursor() as cursor:
+            query = """insert into weather values ('{}',{},{})""".format(city,tem,hud)
+            cursor.execute(query)
+            conn.commit()
     except:
         print("Connection to database failed...")        
         conn.close()
     
-    def insert(city,tem,hud):
-        try:
-            with conn.cursor() as cursor:
-                 query = f"""insert into weather values ('{city}','{tem}','{hud}')"""
-                 cursor.execute(query)
-                 conn.commit()
-        finally:
-            conn.close()
-       
-
 while True :
     location = input('Enter Your City (for exit type end)...: ')
     if location == 'end' :
         break
-    print("Location is {}".format(location))
-    reqF(location)
+    loop = input('Enter Your count ...: ')
+    reqF(location,int(loop))
